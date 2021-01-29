486e2596ca8bf04aa024b148f42f34529742290b
Basovich <frghfqv86>
2021-01-29 02:33:47 +0200

    add repo list

--- src/js/components/github-user/GetGitHubUsers.js
+++ src/js/components/github-user/GetGitHubUsers.js
@@ -3,6 +3,8 @@ import {getTarget} from "../../helper-function/get-target";
 class GetGitHubUsers {
 	constructor() {
 		this.isEmpty = true;
+		this.userData = '';
+		this.userRepoList = '';
 		this.userName = '';
 		this.url = `https://api.github.com/users/`;
 		this.button = document.querySelector('[data-button-add]');
@@ -65,11 +67,18 @@ class GetGitHubUsers {
 			.then( userData => {
 				if (userData) {
 					console.log(userData);
-					this.renderCard(userData);
+					this.userData = userData;
+					return userData.repos_url;
 				} else {
 					this.validate('User does not exist');
 				}
 			})
+			.then( url => this.fetchUserRepos(url))
+			.then( repoList => {
+				this.userRepoList = repoList;
+				this.renderCard();
+			})
+			.catch(error => console.log(error));
 	}
 
 	fetchUserData() {
@@ -80,7 +89,20 @@ class GetGitHubUsers {
 				} else {
 					return false;
 				}
-			});
+			})
+			.catch(error => console.log(error));
+	}
+
+	fetchUserRepos(url) {
+		return fetch(url)
+			.then(response => {
+				if (response.ok) {
+					return response.json();
+				} else {
+					return false;
+				}
+			})
+			.catch(error => console.log(error));
 	}
 
 	validate(text) {
@@ -88,28 +110,25 @@ class GetGitHubUsers {
 		this.errorMessage.textContent = text;
 	}
 
-	renderCard(...userData) {
-		const {avatar_url, login, bio, location, email, html_url} = userData[0];
+	renderCard() {
+		const {avatar_url, login, bio, location, html_url} = this.userData;
 		const card = document.createElement('div');
 		const bioText = bio ? bio : '';
 		const locationText = location ? `From: ${location}` : `From: empty info`;
-		const emailText = email
-			? `Email: <a href="mailto:${email}" class="card-link">${email}</a>`
-			: `Email: <span class="card-link">empty info</span>`;
 		const htmlUrlText = html_url
-			? `GitUrl: <a href="${html_url}" class="card-link" target="_blank">${html_url}</a>`
-			: `GitUrl: <span class="card-link">empty info</span>`;
+			? `GitHubUrl: <a href="${html_url}" class="card-link" target="_blank">${html_url}</a>`
+			: `GitHubUrl: <span class="card-link">empty info</span>`;
 
 		card.className = 'col card-user';
 
 		card.innerHTML = `<div class="card h-100">
 								<div class="w-100 text-end pt-2 pe-2">
-									<button 
-										type="button" 
-										class="btn-close" 
+									<button
+										type="button"
+										class="btn-close"
 										aria-label="Close"
 										data-delete-card></button>
-								</div>								
+								</div>
 								<img src="${avatar_url}"
 									 class="card-img-top rounded-circle m-auto mt-3 mb-3 avatar"
 									 alt="GitHub User avatar">
@@ -118,14 +137,25 @@ class GetGitHubUsers {
 									<p class="card-text text-body">${bioText}</p>
 								</div>
 								<ul class="list-group list-group-flush">
-									<li class="list-group-item text-secondary">${locationText}</li>
-									<li class="list-group-item text-secondary">${emailText}</li>
+									<li class="list-group-item text-secondary">${locationText}</li>							
 									<li class="list-group-item text-secondary">${htmlUrlText}</li>
 								</ul>
+								<p class="text text-muted text-uppercase ps-3 pt-2">Repo List:</p>
+							  	<div class="repos ps-3 pe-3 pt-1 pb-3"></div>							
 							</div>`;
+
+		this.userRepoList.forEach( repo => {
+			const repoBlock = document.createElement('a');
+			repoBlock.href = repo.html_url;
+			repoBlock.setAttribute('target', '_blank');
+			repoBlock.classList.add('badge', 'bg-primary', 'm-1', 'text-light', 'text-decoration-none');
+			repoBlock.textContent = repo.name;
+			card.querySelector('.repos').appendChild(repoBlock);
+		})
 
 		this.addCard(card);
 	}
+
 
 	addCard(card) {
 		if (this.isEmpty) {
